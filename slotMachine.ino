9d1814760f8f645ccb475abc4ee5b51a96fababa
MaxWotsch <max@wotsch.com>
2022-02-09 12:40:12 +0100

    first version of code
    not working

--- slotMachine.ino
+++ slotMachine.ino
@@ -1,7 +1,28 @@
-void setup() {
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+int buttonPin = 2;
+
+void setup()
+{
+  pinMode(buttonPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, FALLING);
+
+  lcd.init();
+  lcd.backlight();
+  delay(300);
 }
 
-void loop() {
+void loop()
+{
+  
+}
 
+void buttonPressed() {
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  delay(1000);
+  lcd.clear();
 }

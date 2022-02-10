e99a75542e5993251ee222fe4d2a713ef4c9a929
MaxWotsch <max@wotsch.com>
2022-02-10 09:10:03 +0100

    first revision of working code
    developped firt code
    no Score calculation

--- slotMachine.ino
+++ slotMachine.ino
@@ -6,28 +6,82 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 const int buttonPin = 2;
 
 boolean buttonIsPressed = false;
+int slot[3];
+int score = 100;
 
 void setup()
 {
   pinMode(buttonPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, FALLING);
+  attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, LOW);
 
   lcd.init();
   lcd.backlight();
   delay(300);
+  generateField();
+  displayScore();
 }
 
 void loop()
 {
   if (buttonIsPressed) {
     buttonIsPressed = false;
-    lcd.setCursor(0,0);
-    lcd.print("Hello, world!");
-    delay(1000);
-    lcd.clear();
+    turn();
   }
 }
 
 void buttonPressed() {
   buttonIsPressed = true;
+}
+
+void generateField() {
+  lcd.setCursor(1,0);
+  lcd.print("|");
+  lcd.setCursor(3,0);
+  lcd.print("|");
+  lcd.setCursor(11,0);
+  lcd.print("Score");
+}
+
+void turn() {
+  if(turnAllowed()) {
+    generateSlot();
+    displayTurn();
+    calculeteScore();
+    displayScore();
+  }
+}
+
+bool turnAllowed() {
+  if (score <= 0) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Score to low :-(");
+    return false;
+  }
+  return true;
+}
+
+void generateSlot() {
+  score--;
+  for (int i = 0; i < 3; i++) {
+    slot[i] = random(0,9);
+  }
+}
+
+void displayTurn() {
+  int i = 0;
+  for (int j = 0; j < 5; j = j+2) {
+    lcd.setCursor(j,0);
+    lcd.print(slot[i]);
+    i++;
+  }
+}
+
+void calculeteScore() {
+  /* code */
+}
+
+void displayScore() {
+  lcd.setCursor(13,1);
+  lcd.print(score);
 }
